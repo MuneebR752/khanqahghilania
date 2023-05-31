254783a5b843b4aa09a371d0e49de7cf5cae859d
Muneeb <muneeburrehman440@gmail.com>
2023-05-31 12:36:41 +0500

    Added Node Server

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const connectDB = require("./config/mongoDB");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const path = require("path");
+app.use(morgan("dev"));
+app.use(cors());
+app.use(express.json());
+dotenv.config({ path: ".env" });
+const route = require("./routes/user");
+connectDB();
+app.use(express.static(path.join(__dirname, "public")));
+app.get("/", (_, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+app.post("/api/signup", route);
+app.post("/api/login", route);
+app.post("/api/auth", route);
+app.post("/api/changePassword", route);
+app.delete("/", route);
+app.listen(5000, () => console.log("Server started on port 5000"));
---
+++ routes/user.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const bcrypt = require("bcrypt");
+const router = express.Router();
+const User = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+router.post("/api/signup", async (req, res) => {
+  const user = {
+    ...req.body,
+    password: await bcrypt.hash(req.body.password, 10),
+  };
+
+  try {
+    let newUser = await User.create(user);
+    let jwtToken = jwt.sign(
+      {
+        id: newUser._id,
+        username: newUser.username,
+        password: newUser.password,
+      },
+      process.env.JWT_SECRET
+    );
+    return res.status(201).json({ message: "User created", token: jwtToken });
+  } catch (error) {
+    if (error.code === 11000) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+    return res.status(500).json({ message: "Server error" });
+  }
+});
+
+router.post("/api/login", async (req, res) => {
+  const user = await User.findOne({ username: req.body.username });
+  if (!user) {
+    return res.status(400).json({ message: "User not found" });
+  }
+  if (await bcrypt.compare(req.body.password, user.password)) {
+    let jwtToken = jwt.sign(
+      { id: user._id, username: user.username, password: user.password },
+      process.env.JWT_SECRET
+    );
+    return res
+      .status(201)
+      .json({ message: "Login Successfully", token: jwtToken });
+  }
+  return res.status(400).json({ message: "Incorrect password" });
+});
+
+router.delete("/", async (req, res) => {
+  await User.deleteMany({}).then((result) => {
+    return res.status(200).json({ message: "All Users deleted" });
+  });
+});
+
+router.post("/api/auth", (req, res) => {
+  jwt.verify(req.body.token, process.env.JWT_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(400).json({ authentication: false });
+    } else {
+      return res.status(200).json({ authentication: true });
+    }
+  });
+});
+
+router.post("/api/changePassword", async (req, res) => {
+  let { current_password, new_password, token } = req.body;
+  let user = jwt.verify(token, process.env.JWT_SECRET);
+  try {
+    if (await bcrypt.compare(current_password, user.password)) {
+      let hashedPassword = await bcrypt.hash(new_password, 10);
+      await User.updateOne(
+        { _id: user.id },
+        { $set: { password: hashedPassword } }
+      );
+      return res
+        .status(200)
+        .json({ success: true, message: "Password changed successfully" });
+    } else {
+      return res
+        .status(400)
+        .json({ success: false, message: "Incorrect password" });
+    }
+  } catch (err) {
+    return res.status(500).json({ message: "Server error", success: false });
+  }
+});
+
+module.exports = router;
