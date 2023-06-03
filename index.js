7aab7e98c6e762b3defd90b1db5c07c6e76ef24d
Muneeb <muneeburrehman440@gmail.com>
2023-06-04 00:35:45 +0500

    min changes

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const UserRouter = require("./routes/user");
 app.use(morgan("dev"));
 app.use(cors());
 
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
